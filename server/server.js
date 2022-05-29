8570f25a487927ff8169f15db6ba0c195058f908
Abarbaneltech <abarbaneltech@gmail.com>
2022-05-30 01:45:39 +0300

    Update server.js
    a first commit update, try out.

--- server/server.js
+++ server/server.js
@@ -8,5 +8,5 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+  console.log(`Server is running at port: ${PORT}`);
 });
