33363799367177eef5f50382816811e4a898c081
Abarbaneltech <abarbaneltech@gmail.com>
2022-05-30 23:42:11 +0300

    connecting mongodb atlas
    connected mongodb atlas cloud to my project

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-const express = require("express");
-
 const app = express();
-const PORT = process.env.PORT || 8000;
-
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server is running at port: ${PORT}`);
+const PORT = process.env.PORT || 8000;
+const URI = process.env.MONGODB_CONNECTION;
+
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+
+  app.listen(PORT, () => {
+    console.log(`Server is running at port: ${PORT}`);
+  });
 });
